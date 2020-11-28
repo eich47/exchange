7c64137f7382089f4a0125ea3d7fa24d2a8bdfa6
Sergey <eich@tut.by>
2020-11-28 19:22:40 +0300

    базовый шаблон для калькулятор, удален автосгенерированый код

---
+++ app/app_exchange/src/components/Calculator.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-container fluid="true">
+    <b-row>
+      <b-col>123</b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+}
+</script>
+
+<style scoped></style>
