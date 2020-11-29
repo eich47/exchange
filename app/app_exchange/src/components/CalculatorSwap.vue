44c19ff42646b642545097488e34bd709e02b022
Sergey <eich@tut.by>
2020-11-29 22:09:55 +0300

    блок "поменять местами"

---
+++ app/app_exchange/src/components/CalculatorSwap.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="image-swap">
+      <img :src="require('@/assets/arrow.jpg')" alt="поменять местами">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CalculatorSwap",
+  }
+</script>
+
+<style scoped>
+
+</style>
