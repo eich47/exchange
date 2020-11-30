3997498c87d15df5c8c98a307957be0b9306f9e8
Sergey <eich@tut.by>
2020-11-30 09:54:37 +0300

    компонент кнопка "обменять"

---
+++ app/app_exchange/src/components/CalculatorButtonExchange.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button class="btn"
+            type="submit"
+            disabled="isDisabled"
+    >Обменять</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CalculatorButtonExchange",
+    props:{
+      isDisabled: {
+        type: Boolean,
+        required: true,
+      },
+    },
+  }
+</script>
+
+<style scoped>
+ .bg-color{
+     background: linear-gradient(140.59deg, #562EC2 29.59%, #A07EFE 73.59%);
+     opacity: 0.8;
+     border-radius: 9px;
+ }
+</style>
