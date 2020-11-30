49702c7d8554a8c43644b0e62131d4d1649c12cf
Sergey <eich@tut.by>
2020-11-30 10:33:31 +0300

    базовый state

--- app/app_exchange/src/store/index.js
+++ app/app_exchange/src/store/index.js
@@ -1,12 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Currency from '../util/currency'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    amountIn: 0, //сколько отдаем
+    amountOut: 0, //сколько получаем
+    isProcessing: false, //идет процесс обмена
+    currency: [
+      {id: 1, text: 'RUB', value: Currency.RUB, img: require('@/assets/rub.jpg')},
+      {id: 2, text: 'USD', value: Currency.USD, img: require('@/assets/usd.jpg')},
+      {id: 3, text: 'BTC', value: Currency.BTC, img: require('@/assets/bnc.png')},
+    ], //доступные валютные пары
+    selectedInCurrency: Currency.RUB, //какую валюту хочет отдать пользователь
+    selectedOutCurrency: Currency.BTC, //какую валюту хочет получить пользователь
+    
   },
   mutations: {
+    setAmountIn(state){
+      this.amountIn = state
+    },
+    setAmountOut(state){
+      this.amountOut = state
+    },
+    setIsProcessing(state){
+      this.isProcessing = state
+    },
   },
   actions: {
   },
---
+++ app/app_exchange/src/util/currency.js
@@ -0,0 +1,5 @@
+export default {
+  RUB: 1,
+  USD: 2,
+  BTC: 3,
+}
